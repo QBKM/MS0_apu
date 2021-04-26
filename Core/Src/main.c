225e315830eed7e7b3be180e1ad9b952ca9f706d
Quentin Bakrim <quentin.bakrim@hotmail.fr>
2021-04-26 10:59:24 +0200

    change comments for doxygen

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,21 +1,14 @@
 /* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                        opensource.org/licenses/BSD-3-Clause
-  *
-  ******************************************************************************
-  */
+/** ************************************************************* *
+ * @file        main.c
+ * @brief       
+ * 
+ * @date        2021-04-26
+ * @author     Quentin Bakrim (quentin.bakrim@hotmail.fr)
+ * 
+ * Mines Space
+ * 
+ * ************************************************************* **/
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
