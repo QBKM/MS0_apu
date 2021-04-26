c60638b46ee704b01933f9f536be13977903929a
Quentin Bakrim <quentin.bakrim@hotmail.fr>
2021-04-26 15:29:58 +0200

    update ds3231

--- Components/hardware/ds3231.c
+++ Components/hardware/ds3231.c
@@ -9,29 +9,103 @@
  * 
  * ************************************************************* **/
 
+
 /* ------------------------------------------------------------- --
-   Includes
+   includes
 -- ------------------------------------------------------------- */
 #include "ds3231.h"
 #include "config_file.h"
 #include "i2c.h"
+
+
 /* ------------------------------------------------------------- --
    defines
 -- ------------------------------------------------------------- */
+/* I2C timeout */
 #ifndef TIMEOUT_I2C
 #define TIMEOUT_I2C 10
 #endif
+
+/* DS3231 address */
+#ifndef DS3231_ADDR
+#define DS3231_ADDR  (0x68 << 1)
+#endif
+
+/* DS3231 registers */
+#define DS3231_REG_TIME         0x00
+#define DS3231_REG_ALARM1       0x07
+#define DS3231_REG_ALARM2       0x0B
+#define DS3231_REG_CONTROL      0x0E
+#define DS3231_REG_STATUS       0x0F
+#define DS3231_REG_TEMP         0x11
+
+#define DS3231_CON_EOSC         0x80
+#define DS3231_CON_BBSQW        0x40
+#define DS3231_CON_CONV         0x20
+#define DS3231_CON_RS2          0x10
+#define DS3231_CON_RS1          0x08
+#define DS3231_CON_INTCN        0x04
+#define DS3231_CON_A2IE         0x02
+#define DS3231_CON_A1IE         0x01
+
+#define DS3231_STA_OSF          0x80
+#define DS3231_STA_32KHZ        0x08
+#define DS3231_STA_BSY          0x04
+#define DS3231_STA_A2F          0x02
+#define DS3231_STA_A1F          0x01
+
+
+/* ------------------------------------------------------------- --
+   types
+-- ------------------------------------------------------------- */
+typedef enum
+{
+  SUNDAY = 1,
+  MONDAY,
+  TUESDAY,
+  WEDNESDAY,
+  THURSDAY,
+  FRIDAY,
+  SATURDAY
+} DS3231_DaysOfWeek;
+
 
 /* ------------------------------------------------------------- --
    Variables
 -- ------------------------------------------------------------- */
 DS3231_t DS3231;
 
+
 /* ------------------------------------------------------------- --
    private prototypes
 -- ------------------------------------------------------------- */
 static uint8_t B2D(uint8_t bcd);
 static uint8_t D2B(uint8_t decimal);
+
+/* ============================================================= ==
+   private functions
+== ============================================================= */
+/** ************************************************************* *
+ * @brief      Convert the bcd to decimal
+ * 
+ * @param      bcd 
+ * @return     uint8_t 
+ * ************************************************************* **/
+static uint8_t B2D(uint8_t bcd)
+{
+	return (bcd >> 4) * 10 + (bcd & 0x0F);
+}
+
+/** ************************************************************* *
+ * @brief      Convert the decimal to bcd
+ * 
+ * @param      decimal 
+ * @return     uint8_t 
+ * ************************************************************* **/
+static uint8_t D2B(uint8_t decimal)
+{
+	return (((decimal / 10) << 4) | (decimal % 10));
+}
 
 
 /* ============================================================= ==
@@ -129,27 +203,6 @@ uint8_t DS3231_Set_Time()
 	return HAL_OK;
 }
 
-/** ************************************************************* *
- * @brief      Convert the bcd to decimal
- * 
- * @param      bcd 
- * @return     uint8_t 
- * ************************************************************* **/
-static uint8_t B2D(uint8_t bcd)
-{
-	return (bcd >> 4) * 10 + (bcd & 0x0F);
-}
-
-/** ************************************************************* *
- * @brief      Convert the decimal to bcd
- * 
- * @param      decimal 
- * @return     uint8_t 
- * ************************************************************* **/
-static uint8_t D2B(uint8_t decimal)
-{
-	return (((decimal / 10) << 4) | (decimal % 10));
-}
 
 /* ------------------------------------------------------------- --
    end of file
--- Components/hardware/inc/ds3231.h
+++ Components/hardware/inc/ds3231.h
@@ -14,60 +14,13 @@
 
 
 /* ------------------------------------------------------------- --
-   Includes
+   includes
 -- ------------------------------------------------------------- */
 #include "stdint.h"
 
 
 /* ------------------------------------------------------------- --
-   Define
--- ------------------------------------------------------------- */
-/* DS3231 address */
-#ifndef DS3231_ADDR
-#define DS3231_ADDR  (0x68 << 1)
-#endif
-
-/* DS3231 registers */
-#define DS3231_REG_TIME         0x00
-#define DS3231_REG_ALARM1       0x07
-#define DS3231_REG_ALARM2       0x0B
-#define DS3231_REG_CONTROL      0x0E
-#define DS3231_REG_STATUS       0x0F
-#define DS3231_REG_TEMP         0x11
-
-#define DS3231_CON_EOSC         0x80
-#define DS3231_CON_BBSQW        0x40
-#define DS3231_CON_CONV         0x20
-#define DS3231_CON_RS2          0x10
-#define DS3231_CON_RS1          0x08
-#define DS3231_CON_INTCN        0x04
-#define DS3231_CON_A2IE         0x02
-#define DS3231_CON_A1IE         0x01
-
-#define DS3231_STA_OSF          0x80
-#define DS3231_STA_32KHZ        0x08
-#define DS3231_STA_BSY          0x04
-#define DS3231_STA_A2F          0x02
-#define DS3231_STA_A1F          0x01
-
-
-/* ------------------------------------------------------------- --
-   Enumerates
--- ------------------------------------------------------------- */
-typedef enum
-{
-  SUNDAY = 1,
-  MONDAY,
-  TUESDAY,
-  WEDNESDAY,
-  THURSDAY,
-  FRIDAY,
-  SATURDAY
-} DS3231_DaysOfWeek;
-
-
-/* ------------------------------------------------------------- --
-   Structures
+   defines
 -- ------------------------------------------------------------- */
 typedef struct
 {
@@ -84,22 +37,17 @@ typedef struct
 
 
 /* ------------------------------------------------------------- --
-   Functions
+   functions
 -- ------------------------------------------------------------- */
-uint8_t DS3231_Init(void);
-
+uint8_t  DS3231_Init(void);
 DS3231_t DS3231_Get_Struct(void);
-
-uint8_t DS3231_Read_All(void);
-uint8_t DS3231_Read_Time(void);
-uint8_t DS3231_Read_Temperature(void);
-
-uint8_t DS3231_Set_Time(void);
+uint8_t  DS3231_Read_All(void);
+uint8_t  DS3231_Read_Time(void);
+uint8_t  DS3231_Read_Temperature(void);
+uint8_t  DS3231_Set_Time(void);
 
 
 #endif /* __DS3231_H__ */
-
-
 /* ------------------------------------------------------------- --
-   End of files
+   end of files
 -- ------------------------------------------------------------- */
