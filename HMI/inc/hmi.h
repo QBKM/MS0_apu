844899c763efbbece920c45bcd5f0c4f69e8e477
Quentin Bakrim <quentin.bakrim@hotmail.fr>
2021-04-27 23:11:17 +0200

    add data log display

--- HMI/inc/hmi.h
+++ HMI/inc/hmi.h
@@ -17,6 +17,9 @@
 -- ------------------------------------------------------------- */
 #include "err_manager.h"
 
+#include "ds3231.h"
+#include "mpu6050.h"
+#include "bmp280.h"
 
 /* ------------------------------------------------------------- --
    types
@@ -32,8 +35,13 @@ typedef enum
    function prototypes
 -- ------------------------------------------------------------- */
 uint8_t HMI_OLED_display_bitmap(const unsigned char* bitmap, uint32_t time, HMI_AfterDisp_t action);
-uint8_t HMI_OLED_display_init_log(HW_init_t HW_init, uint32_t time, HMI_AfterDisp_t action);
+uint8_t HMI_OLED_display_init_log(HW_status_t HW_init, uint32_t time, HMI_AfterDisp_t action);
 
+uint8_t HMI_OLED_display_data_log(void);
+void    HMI_OLED_display_data_log_failed(uint8_t HEIGHT);
+void    HMI_OLED_display_data_log_time(DS3231_t TIME);
+void    HMI_OLED_display_data_log_press(BMP280_t PRESS);
+void    HMI_OLED_display_data_log_angle(MPU6050_t ANGLE);
 
 #endif
 /* ------------------------------------------------------------- --
